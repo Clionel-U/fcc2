eb567ee29d495d6556ee98b205ee9f1f20257d5d
Clionel-U <clionelus@gmail.com>
2025-03-14 13:56:36 +0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,39 +1,25 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname+"/views/index.html")
+})
+app.use("/public", express.static(__dirname + "/public"))
 
+app.use('/', (req, res, next)=>{
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
 
+app.get('/json', (req, res)=>{
+  let msg = "Hello json";
+  if (process.env['MESSAGE_STYLE'] === 'uppercase'){
+    msg=msg.toUpperCase();
+  }
+  res.json({"message":msg});
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
