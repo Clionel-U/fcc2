fa64e359c72f398da23afea2bc35595a76992324
Clionel-U <clionelus@gmail.com>
2025-03-18 10:40:19 +0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,25 +1,44 @@
 require('dotenv').config();
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
 
 console.log("Hello World");
+
+app.use((req,res,next)=>{
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
 
 app.get('/', (req, res)=>{
   res.sendFile(__dirname+"/views/index.html")
 })
 app.use("/public", express.static(__dirname + "/public"))
 
-app.use('/', (req, res, next)=>{
-  console.log(`${req.method} ${req.path} - ${req.ip}`)
-  next()
-})
-
 app.get('/json', (req, res)=>{
   let msg = "Hello json";
-  if (process.env['MESSAGE_STYLE'] === 'uppercase'){
-    msg=msg.toUpperCase();
+  if (process.env.MESSAGE_STYLE === 'uppercase'){
+    msg = msg.toUpperCase();
   }
-  res.json({"message":msg});
+  const data = {"message": msg};
+  res.json(data);
 })
+console.log(process.env.MESSAGE_STYLE)
+
+app.get('/now', (req, res, next)=>{
+  req.time = new Date().toString();
+  next();
+},
+ (req, res)=>{
+   res.json({time: req.time})
+ }       
+)
+
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
+
 
 module.exports = app;
